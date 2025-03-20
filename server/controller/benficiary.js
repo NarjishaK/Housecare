c6629195013c0b8f29930d9372871492d51e9b9e
Narjisha K <144308345+NarjishaK@users.noreply.github.com>
2025-03-20 22:32:04 +0530

    update

--- server/controller/benficiary.js
+++ server/controller/benficiary.js
@@ -440,7 +440,7 @@ exports.importFromExcel = async (req, res) => {
   }
 };
 
-async (req, res) => {
+exports.importBenificiariesFromExcel = async (req, res) => {
   try {
     if (!req.file) {
       return res.status(400).json({ error: "No file uploaded" });
